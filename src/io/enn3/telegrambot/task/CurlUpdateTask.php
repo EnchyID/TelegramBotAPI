8850c47e6ee33fd6388b4387c49c3795996b2903
FrogasQ <91828204+FrogasQ@users.noreply.github.com>
2022-05-01 08:51:01 +0800

    Update CurlUpdateTask.php

--- src/io/enn3/telegrambot/task/CurlUpdateTask.php
+++ src/io/enn3/telegrambot/task/CurlUpdateTask.php
@@ -8,16 +8,25 @@ use io\enn3\telegrambot\TelegramBot;
 class CurlUpdateTask extends Task {
 	
 	private $plugin;
+	private $url;
+	private $method;
+	private $params;
+    private $type;
+	private $opts;
 	
-	public function __construct(TelegramBot $plugin){
-		$this->plugin = $plugin;
+	public function __construct(TelegramBot $plugin, $url, $method, $type = "GET", $params = array(), $opts = array()){
+	    $this->plugin = $plugin;
+		$this->url = $url;
+		$this->method = $method;
+		$this->params = $params;
+		$this->opts = $opts;
 	}
 	
-	public function getLoader() : TelegramBot {
-		return $this->plugin;
+	public function getLoader(): TelegramBot{
+	    return $this->plugin;
 	}
 	
 	public function onRun() : void {
-	    $this->getLoader()->getUpdates()["result"];
+	    $this->getLoader()->request($this->url . $this->method, $this->type, $this->params);
 	}
 }
